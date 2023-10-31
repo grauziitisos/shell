be584adba610673a31d49e074f8fa4603bc71ba4
wilpeers <wilpeers@gmail.com>
2023-11-01 01:21:28 +0200

    bug fix: when there is no leading zero two folders are created bc split on the space

--- mkdirdt.cmd
+++ mkdirdt.cmd
@@ -2,4 +2,5 @@
 REM create folder ending witth datetime IFF yyyy-MM-DD is your date format. otherwise change start->length pairs according to echo %date%
 setlocal EnableDelayedExpansion
 SET DT=%1%
-mkdir !DT!!DATE:~0,4!-!DATE:~5,2!-!DATE:~8,2!_!TIME:~0,2!-!TIME:~3,2!-!TIME:~6,2!
+SET TMWITHZERO=%TIME: =0%
+mkdir !DT!!DATE:~0,4!-!DATE:~5,2!-!DATE:~8,2!_!TMWITHZERO:~0,2!-!TMWITHZERO:~3,2!-!TMWITHZERO:~6,2!
